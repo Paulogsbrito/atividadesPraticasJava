d2e7280f33b7e514fa5a6e8d933e6c92dac4afe1
Paulo <paulogsbrito@gmail.com>
2026-04-17 19:46:44 -0300

    Exercicio 01 finalizado

---
+++ src/praticasJava/Exercicio01.java
@@ -0,0 +1,34 @@
+package praticasJava;
+
+import java.util.Scanner;
+
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+		//abertura e criação do scanner de leitura + declaração das variaveis
+		Scanner leia = new Scanner(System.in);
+		// tipo de variavel abaixo que será lida
+		float salario;
+		float abono;
+		float novoSalario;
+		
+		
+		// Digite seu salario (Recebimento de dados)
+		System.out.println("Por gentileza digite seu salario: ");
+		
+		//acão de leitura do salario digitado
+		salario = leia.nextFloat();
+		
+		System.out.println("Por gentileza digite seu Abono: ");
+		
+		abono = leia.nextFloat();
+		
+		novoSalario = salario + abono;
+		
+		System.out.printf("Seu novo Salario é: " + novoSalario + " Reais");	
+
+	}
+
+}
